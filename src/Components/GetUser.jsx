19dfd0a81c9d4739a2de5b0a68bdf4130c11dc20
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-07 13:50:22 +0530

    Profile Card Created

--- src/Components/GetUser.jsx
+++ src/Components/GetUser.jsx
@@ -30,7 +30,7 @@ const GetUser = () => {
 
     return (
         <>
-            <div className="main">
+            <div className="main flex flex-col">
                 <div>
                     <input className="bg-slate-500" type="text"
                         onChange={debounce}
@@ -39,12 +39,12 @@ const GetUser = () => {
                 {validate ? <div>some</div> :
                     (userData.user.error === 404 ? <div>error</div>
                         : (userData.loading === true ? "loading" :
-                            <div className="bg-[#1b1b1b] flex flex-col items-center w-fit p-4 rounded-md">
-                                <img className="w-[20rem] h-[20rem] rounded shadow-md" src={image} alt="" />
-                                <div className="text-white text-xl tracking-wide text-center my-5 p-3 bg-[#232323] min-w-[20rem] rounded">
-                                    <p className=" bg-[#2e2e2e] min-w-[19rem] rounded p-[6px]">{name}</p>
-                                    <p className="bg-[#2e2e2e] min-w-[19rem] rounded my-[7px] p-[6px]">Id: {id}</p>
-                                    <p className="bg-[#2e2e2e] min-w-[19rem] rounded p-[6px]">UserName: {username}</p>
+                            <div className="bg-[#1b1b1b] flex flex-col items-center w-fit p-4 rounded-md m-4">
+                                <img className="w-[20.5rem] h-[20.5rem] rounded shadow-md" src={image} alt="" />
+                                <div className="text-white text-[1.2rem] tracking-wide text-center my-5 p-3 py-5 bg-[#232323] min-w-[20rem] rounded">
+                                    <p className=" bg-[#2e2e2e] min-w-[19rem] rounded p-[7px]">{name}</p>
+                                    <p className="bg-[#2e2e2e] min-w-[19rem] rounded my-[8px] p-[7px]">Id: {id}</p>
+                                    <p className="bg-[#2e2e2e] min-w-[19rem] rounded p-[7px]">UserName: {username}</p>
                                 </div></div>
                         ))}</div>
         </>
